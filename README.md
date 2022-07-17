c20b469f9693e106fda71bb40159d92c9251e05a
Ulrich Lehner <me@ulrichlehner.com>
2022-07-17 16:37:44 +0200

    improvement: added basic business logic to scrape NÖ Netz smartmeter data

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# homebase
+# Homebase
+
 Raspberry Pi based home monitoring dashboard
+
+## Setup
+
+### `./webscraper/.env`
+
+```bash
+USERNAME=...
+PASSWORD=...
+METER_ID=...
+# DAY=2022-7-16
+```
---
+++ webscraper/main.py
@@ -0,0 +1,85 @@
+import logging
+import os
+import urllib.parse
+from datetime import datetime
+
+import requests
+from influxdb import InfluxDBClient
+
+
+def main():
+    api_base_url = 'https://smartmeter.netz-noe.at/orchestration/'
+    db_name = 'smartmeter'
+    day = os.environ['DAY'] if 'DAY' in os.environ else datetime.now().strftime(
+        '%Y-%-m-%-d')
+    logging.getLogger().setLevel(logging.INFO)
+    session = requests.Session()
+    response = session.post(
+        api_base_url + 'Authentication/Login',
+        json={
+            'user': os.environ['USERNAME'],
+            'pwd': os.environ['PASSWORD']
+        }
+    )
+    if response.status_code != 200:
+        logging.error('Login failed')
+        exit(1)
+
+    logging.info('Login successful')
+
+    response = session.get(
+        api_base_url +
+        f"ConsumptionRecord/Day?meterId={urllib.parse.quote(os.environ['METER_ID'])}&day={day}"
+    )
+
+    if response.status_code != 200:
+        session.get(api_base_url + 'Authentication/Logout')
+        logging.error('Fetching meter data failed')
+        exit(1)
+
+    json = response.json()
+
+    if not 'peakDemandTimes' in json or len(json['peakDemandTimes']) == 0:
+        session.get(api_base_url + 'Authentication/Logout')
+        logging.warning(f"No measurement data for {day}")
+        exit(1)
+
+    client = InfluxDBClient(
+        host=os.environ['INFLUXDB_HOST'],
+        port=os.environ['INFLUXDB_PORT']
+    )
+
+    if not db_name in list(map(lambda db: db['name'], client.get_list_database())):
+        client.create_database(db_name)
+        logging.info(f"Created DB '{db_name}'")
+
+    client.switch_database(db_name)
+
+    times = [{'time': x, 'index': i}
+             for i, x in enumerate(json['peakDemandTimes'])]
+    if client.write_points(
+        list(
+            map(lambda t: {
+                'measurement': 'meteredPeakDemands',
+                'tags': {'meterId': os.environ['METER_ID']},
+                'time': t['time'],
+                'fields': {'value': json['meteredPeakDemands'][t['index']]}
+            }, times)
+        )
+        +
+        list(
+            map(lambda t: {
+                'measurement': 'meteredValues',
+                'tags': {'meterId': os.environ['METER_ID']},
+                'time': t['time'],
+                'fields': {'value': json['meteredValues'][t['index']]}
+            }, times)
+        )
+    ):
+        logging.info('Stored measurements in DB')
+
+    session.get(api_base_url + 'Authentication/Logout')
+
+
+if __name__ == '__main__':
+    main()
