0aa3e3b40388e5b7eb3a9f7c328f1554a424bbdf
Ulrich Lehner <me@ulrichlehner.com>
2022-07-22 16:58:44 +0200

    feat: added cron script to operate Kindle display

---
+++ kindle_cron.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+KINDLE_IP=192.168.15.244
+
+# Disable Kindle services
+ssh root@$KINDLE_IP /sbin/stop lab126_gui # Basic UI as it's not needed
+ssh root@$KINDLE_IP /sbin/stop powerd # Should prevent screensaver and background LEDs
+# There might be other suitable to stop, see https://github.com/mattzzw/kindle-gphotos/blob/master/kindle-gphotos.sh
+# stop otaupd
+# stop phd
+# stop tmd
+# stop x
+# stop todo
+# stop mcsd
+# stop archive
+# stop dynconfig
+# stop dpmd
+# stop appmgrd
+# stop stackdumpd
+
+# Display chart on Kindle
+scp "${script_path}/export/current.png" root@$KINDLE_IP:/var/tmp/root
+ssh root@$KINDLE_IP /usr/sbin/eips -f -g /var/tmp/root/current.png
