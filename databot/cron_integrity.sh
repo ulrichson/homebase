d619fa53037ef4a461d1c53d7c121d533d074247
Ulrich Lehner <me@ulrichlehner.com>
2023-09-22 14:39:00 +0200

    feat: added doctor command for integrity check and more robust loading

---
+++ databot/cron_integrity.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+script_path=$(dirname "$(readlink -f "$0")")
+source "${script_path}/.env.sh"
+PATH=/usr/local/bin:$PATH
+cd /app
+databot --continue-migration
--- databot/index.ts
+++ databot/index.ts
@@ -3,9 +3,19 @@
 import { InfluxDB, Point } from '@influxdata/influxdb-client';
 import { Command } from 'commander';
 import * as dotenv from 'dotenv';
-import moment from 'moment-timezone';
+import { DateTime } from 'luxon';
+import moment, { Moment } from 'moment-timezone';
 import fs from 'node:fs';
 import puppeteer, { Page } from 'puppeteer';
+
+const dateFormat = 'DD.MM.YYYY';
+const maxFailedAttempts = 5;
+
+async function sleep(ms: number) {
+  return new Promise<void>((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
 function hasDockerEnv() {
   try {
@@ -27,7 +37,19 @@ function hasDockerCGroup() {
 function isDocker() {
   return hasDockerEnv() || hasDockerCGroup();
 }
-const dateFormat = 'DD.MM.YYYY';
+
+function getDatetimeRange(weeksBack: number, date: DateTime, tz: string) {
+  const format = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+  date = date.set({ hour: 0, minute: 0, second: 0, millisecond: 0 });
+  const start = date.minus({ days: date.weekday, weeks: weeksBack });
+  const stop = start.plus({ weeks: 1 });
+  return {
+    start: start.toUTC().toFormat(format),
+    stop: stop.toUTC().toFormat(format),
+
+    format,
+  };
+}
 
 interface Config {
   meterId: string;
@@ -67,19 +89,52 @@ class Bot {
   }
 
   /**
-   * Loads the consumption data for the given day
+   * Loads the consumption data for the given day and stores it in the DB
    * @param day - Date with format DD.MM.YYYY
    *
-   * @returns Whether the data was loaded successfully
+   * @return The number of saved data points or `-1` if an error occured
    */
   async load(day: string): Promise<number> {
     let ret = 0;
+    try {
+      const data = await this.fetch(day);
+      if (!data) {
+        throw 'No data was returned';
+      }
+      // console.log(
+      //   data
+      //     .filter((row) => (<any>row).name === 'meteredValues')
+      //     .map((row) => `${(<any>row).time}: ${row.fields.value}`)
+      // );
+      ret = data.length;
+      const writeApi = this.influxDb.getWriteApi(
+        this.config.influxOrg,
+        this.config.influxBucket,
+        's'
+      );
+      writeApi.writePoints(data);
+      writeApi.close();
+      console.info(`Stored measurements in DB for ${day}`);
+    } catch {
+      return -1;
+    }
+    return ret;
+  }
+
+  /**
+   * Fetches the consumption data for the given day
+   * @param day - Date with format DD.MM.YYYY
+   *
+   * @return The data points, or null if it failed
+   */
+  async fetch(day: string) {
+    let ret: Point[] | null = null;
     const browser = isDocker()
       ? await puppeteer.launch({
           executablePath: '/usr/bin/chromium',
           args: ['--no-sandbox', '--disable-setuid-sandbox'],
         })
-      : await puppeteer.launch({ headless: true });
+      : await puppeteer.launch({ headless: !this.config.debug });
     const page = await browser.newPage();
 
     page.on('console', (msg) => console.debug('BROWSER DEBUG: ', msg.text()));
@@ -88,10 +143,12 @@ class Bot {
     await page.goto('https://www.linznetz.at', {
       waitUntil: 'domcontentloaded',
     });
-    await page.click('#loginFormTemplateHeader\\:doLogin');
-    await page.waitForNavigation({
-      waitUntil: 'domcontentloaded',
-    });
+    await Promise.all([
+      page.waitForNavigation({
+        waitUntil: 'domcontentloaded',
+      }),
+      page.click('#loginFormTemplateHeader\\:doLogin'),
+    ]);
 
     console.debug('Enter credentials');
     await page.type('#username', this.config.username);
@@ -103,7 +160,7 @@ class Bot {
     );
     await page.waitForNavigation();
 
-    console.info('Login successful');
+    console.debug('Login successful');
 
     console.debug('Navigate to consumption page');
     await page.goto(
@@ -160,10 +217,15 @@ class Bot {
         return response.request().url().includes('/consumption.jsf');
       });
       console.debug('Reset pagination');
-      page.click('.ui-paginator-first');
-      await page.waitForResponse((response) => {
-        return response.request().url().includes('/consumption.jsf');
-      });
+      const disabledResetPagiationLink = await page.$(
+        '.ui-paginator-first.ui-state-disabled'
+      );
+      if (!disabledResetPagiationLink) {
+        await page.click('.ui-paginator-first');
+        await page.waitForResponse((response) => {
+          return response.request().url().includes('/consumption.jsf');
+        });
+      }
 
       const meteredPeakDemandsDataTable = await this.downloadResult(
         page /*'Leistung'*/
@@ -189,19 +251,7 @@ class Bot {
             .floatField('value', row[valueKey] ?? row[substituteValueKey]);
         }),
       ];
-      ret = data.length;
-      const writeApi = this.influxDb.getWriteApi(
-        this.config.influxOrg,
-        this.config.influxBucket,
-        's'
-      );
-      // console.log(
-      //   meteredValuesDataTable.headers,
-      //   meteredPeakDemandsDataTable.headers
-      // );
-      writeApi.writePoints(data);
-      writeApi.close();
-      console.info(`Stored measurements in DB for ${day}`);
+      ret = data;
     } catch (err) {
       console.warn(`No measurement data for ${day}`);
       console.debug((<Error>err).stack);
@@ -214,7 +264,7 @@ class Bot {
           });
         } catch {}
       }
-      return 0;
+      return null;
     }
 
     try {
@@ -227,7 +277,7 @@ class Bot {
       );
       await browser.close();
 
-      console.info('Logout successful');
+      console.debug('Logout successful');
     } catch (err) {
       console.warn(`Cannot logout`);
       console.debug((<Error>err).stack);
@@ -334,89 +384,303 @@ class Bot {
     return { headers, rows };
   }
 }
+async function migrate({
+  bot,
+  config,
+  limitDaysBack,
+  from = moment(),
+}: {
+  bot: Bot;
+  config: Config;
+  limitDaysBack?: number;
+  from?: Moment;
+}) {
+  let deltaDays = 0;
+  let hasNext = true;
+  let failedAttempts = 0;
+  let allowedEmptyDays = 7;
 
-async function main() {
-  async function migrate({
-    bot,
-    config,
-    limitDaysBack,
-  }: {
-    bot: Bot;
-    config: Config;
-    limitDaysBack?: number;
-  }) {
-    let deltaDays = 1;
-    let hasNext = true;
-    const maxFailedAttempts = 3;
+  console.info(
+    'Loading past measurements' +
+      (typeof limitDaysBack !== 'undefined'
+        ? ` of last ${limitDaysBack} day${limitDaysBack !== 1 ? 's' : ''}`
+        : '') +
+      ' back from ' +
+      from.tz(config.tz).format(dateFormat)
+  );
+
+  while (hasNext) {
+    const day = from
+      .clone()
+      .tz(config.tz)
+      .subtract(deltaDays, 'days')
+      .format(dateFormat);
+
+    await sleep(failedAttempts > 0 ? 5000 : 300); // Avoid rate limiting
+    if (failedAttempts > 0) {
+      console.log(`Retrying for ${day} ...`);
+    }
+    const result = await bot.load(day);
+    if (result === 0) {
+      allowedEmptyDays -= 1;
+      deltaDays += 1;
+      failedAttempts = 0;
+    } else if (result > 0) {
+      deltaDays += 1;
+      failedAttempts = 0;
+    } else {
+      failedAttempts += 1;
+    }
+
+    if (failedAttempts > maxFailedAttempts || allowedEmptyDays < 0) {
+      console.info('Stopping since no more data is present');
+      hasNext = false;
+    }
+
+    if (typeof limitDaysBack !== 'undefined' && deltaDays >= limitDaysBack) {
+      hasNext = false;
+    }
+  }
+}
+
+async function continueMigration({
+  bot,
+  config,
+  influxDb,
+}: {
+  bot: Bot;
+  config: Config;
+  influxDb: InfluxDB;
+}) {
+  const queryApi = influxDb.getQueryApi(config.influxOrg);
+  const fluxQuery = `
+      from(bucket: "${config.influxBucket}")
+      |> range(start: 0)
+      |> filter(fn: (r) => r._measurement == "meteredValues" and r._field == "value")
+      |> first()
+    `;
+  const response = await queryApi.collectRows(fluxQuery);
+  if (response.length === 0) {
+    console.warn('No date present to continue migration');
+    return;
+  } else {
+    let from = moment((<any>response[0])['_time']);
+    const bufferDays =
+      from < moment().subtract(2, 'days')
+        ? 2
+        : from < moment().subtract(1, 'days')
+        ? 1
+        : 0;
+    from.add(bufferDays, 'days');
+    await migrate({ bot, config, from });
+  }
+}
+
+async function update({
+  bot,
+  config,
+  influxDb,
+}: {
+  bot: Bot;
+  config: Config;
+  influxDb: InfluxDB;
+}) {
+  const queryApi = influxDb.getQueryApi(config.influxOrg);
+  const fluxQuery = `
+      from(bucket: "${config.influxBucket}")
+      |> range(start: 0)
+      |> filter(fn: (r) => r._measurement == "meteredValues" and r._field == "value")
+      |> last()
+    `;
+  const response = await queryApi.collectRows(fluxQuery);
+  if (response.length === 0) {
+    await migrate({ bot, config });
+  } else {
+    console.log('Loading latest measurement(s)');
+    let dt = moment((<any>response[0])['_time']).tz(config.tz);
+    const endOfToday = moment()
+      .tz(config.tz)
+      .set('hours', 0)
+      .set('minutes', 0)
+      .add(1, 'days');
     let failedAttempts = 0;
-    let allowedIncompleteDays = 7;
-    console.info(
-      'Starting migrating old measurements' +
-        (typeof limitDaysBack !== 'undefined'
-          ? ` of last ${limitDaysBack} day${limitDaysBack !== 1 ? 's' : ''}`
-          : '')
-    );
-    while (hasNext) {
-      const day = moment()
-        .tz(config.tz)
-        .subtract(deltaDays, 'days')
-        .format(dateFormat);
-      try {
-        if (
-          (await bot.load(day)) !==
-          24 /* hours a day */ * 4 /* quarter hours */ * 2 /* data rows */
-        ) {
-          allowedIncompleteDays -= 1;
-        }
-        deltaDays += 1;
-      } catch (err) {
-        failedAttempts += 1;
-        console.error(err);
+    while (dt <= endOfToday && failedAttempts <= maxFailedAttempts) {
+      const day = dt.format(dateFormat);
+      console.log(day, endOfToday.format(dateFormat));
+      if (failedAttempts > 0) {
+        console.log(`Fetching measurements for ${day} failed, will retry ...`);
       }
-
-      if (failedAttempts >= maxFailedAttempts || allowedIncompleteDays < 0) {
-        console.debug('Stopping migration since no more data is present');
-        hasNext = false;
-      }
-
-      if (typeof limitDaysBack !== 'undefined' && deltaDays > limitDaysBack) {
-        hasNext = false;
-      }
-    }
-  }
-
-  async function update({
-    bot,
-    config,
-    influxDb,
-  }: {
-    bot: Bot;
-    config: Config;
-    influxDb: InfluxDB;
-  }) {
-    try {
-      const queryApi = influxDb.getQueryApi(config.influxOrg);
-      const fluxQuery = `
-        from(bucket: "${config.influxBucket}")
-        |> range(start: 0)
-        |> filter(fn: (r) => r._measurement == "meteredValues" and r._field == "value")
-        |> last()
-      `;
-      const response = await queryApi.collectRows(fluxQuery);
-      if (response.length === 0) {
-        await migrate({ bot, config });
+      await sleep(failedAttempts > 0 ? 5000 : 300); // Avoid rate limiting
+      const result = await bot.load(day);
+      if (result < 0) {
+        failedAttempts++;
       } else {
-        let dt = moment((<any>response[0])['_time']).tz(config.tz);
-        while (dt <= moment().tz(config.tz)) {
-          await bot.load(dt.format(dateFormat));
-          dt.add(1, 'days');
+        failedAttempts = 0;
+        dt.add(1, 'days');
+      }
+    }
+
+    // Re-load older measurements in case they were updated
+    await migrate({
+      bot,
+      config,
+      from: moment((<any>response[0])['_time']).subtract(1, 'days'),
+      limitDaysBack: 7,
+    });
+  }
+}
+
+async function doctor({
+  bot,
+  config,
+  influxDb,
+  limitDaysBack,
+}: {
+  bot: Bot;
+  config: Config;
+  influxDb: InfluxDB;
+  limitDaysBack?: number;
+}) {
+  try {
+    const queryApi = influxDb.getQueryApi(config.influxOrg);
+    let fluxQuery = `
+      from(bucket: "${config.influxBucket}")
+      |> range(start: 0)
+      |> filter(fn: (r) => r._measurement == "meteredValues" and r._field == "value")
+      |> last()
+    `;
+    let response: any[] = await queryApi.collectRows(fluxQuery);
+    if (response.length === 0) {
+      console.log('No data present in DB to check');
+      return;
+    }
+    let dt = moment((<any>response[0])['_time']).tz(config.tz);
+    // while (dt <= moment().tz(config.tz)) {
+    //   await bot.load(dt.format(dateFormat));
+    //   dt.add(1, 'days');
+    // }
+
+    console.log(`Starting integrity check back from ${dt.format(dateFormat)}`);
+
+    let weeksBack = 1;
+    let hasNext = true;
+    // let zeroValues = 0;
+    const dbResult = new Map<string, number>();
+
+    while (hasNext) {
+      // Check data in weekly chunks and compare it data
+      const { start, stop, format } = getDatetimeRange(
+        weeksBack,
+        DateTime.fromJSDate(dt.toDate()),
+        config.tz
+      );
+
+      fluxQuery = `
+        from(bucket: "${config.influxBucket}")
+          |> range(start: ${start}, stop: ${stop})
+          |> filter(fn: (r) =>  r._measurement == "meteredValues" and r._field == "value")
+          |> aggregateWindow(every: 15m, offset: 1ns, fn: mean)
+      `;
+      // console.log(fluxQuery);
+      response = await queryApi.collectRows(fluxQuery);
+      // console.log({
+      //   response: response.map(
+      //     (r: any) =>
+      //       `${DateTime.fromJSDate(new Date(r._time))
+      //         .toUTC()
+      //         .toFormat(format)}: ${r._value}`
+      //   ),
+      // });
+      response.pop(); // Delete last item since it's empty, not sure how the query would be correct
+      response.forEach((r: any) => {
+        const key = DateTime.fromJSDate(new Date(r._time))
+          .toUTC()
+          .toFormat(format);
+        dbResult.set(key, Number(r._value));
+      });
+
+      if (dbResult.size === 0) {
+        hasNext = false;
+        console.log('No data in DB to check');
+        continue;
+      }
+
+      for (let i = 0; i < 7; i++) {
+        const day = moment
+          .tz(stop, config.tz)
+          .subtract(i + 1, 'days')
+          .format(dateFormat);
+
+        console.log('Testing integrity on ' + day);
+        let data: Point[] | null = null;
+        let failedAttempts = 0;
+
+        while (failedAttempts <= maxFailedAttempts && data == null) {
+          if (failedAttempts > 0) {
+            console.log(`Retrying for ${day} ...`);
+          }
+          await sleep(failedAttempts > 0 ? 5000 : 300); // Avoid rate limiting
+          data = await bot.fetch(day);
+          if (data == null) {
+            failedAttempts++;
+          } else {
+            failedAttempts = 0;
+          }
+        }
+        let hasErrors = false;
+
+        data
+          ?.filter((row) => (<any>row).name === 'meteredValues')
+          .forEach((row) => {
+            const key = DateTime.fromJSDate(
+              new Date((<any>row).time.toString())
+            )
+              .toUTC()
+              .toFormat(format);
+            // console.log(key + ': ', res.get(key));
+            // if (dbResult.get(key)) {
+            //   console.log(
+            //     `Matched - ${key} = ${dbResult.get(key)} vs. ${Number(
+            //       row.fields.value
+            //     )}`
+            //   );
+            // }
+            const dbVal = ('' + dbResult.get(key)).trim();
+            const webVal = ('' + row.fields.value).trim();
+            if (dbResult.has(key)) {
+              if (dbVal != webVal) {
+                console.warn(
+                  `Mismatch DB/web on ${key}:\t${dbVal}\tvs.\t${webVal}`
+                );
+                hasErrors ||= true;
+              }
+            } else {
+              console.log(`${key} not found in DB`);
+            }
+          });
+
+        if (!hasErrors) {
+          console.log('Everything is OK on ' + day);
         }
       }
-    } catch (err) {
-      console.error((<Error>err).message);
-    }
-  }
 
+      weeksBack++;
+      hasNext = response.length > 0;
+
+      if (
+        typeof limitDaysBack !== 'undefined' &&
+        weeksBack * 7 >= limitDaysBack
+      ) {
+        hasNext = false;
+      }
+    }
+  } catch (err) {
+    console.error((<Error>err).message);
+  }
+}
+
+//#region Main
+async function main() {
   try {
     const program = new Command();
     program
@@ -427,6 +691,11 @@ async function main() {
       )
       .option('-c, --config <path>', 'Path to config file')
       .option('-d, --debug', 'Enable debug logging', false)
+      .option('--doctor', 'Check measurements integrity')
+      .option(
+        '--continue-migration',
+        'Continues migration from furthest measurement in the past'
+      )
       .parse();
 
     const options = program.opts();
@@ -471,6 +740,15 @@ async function main() {
 
     if (options.migrate) {
       await migrate({ bot, config, limitDaysBack: options.limit ?? undefined });
+    } else if (options.doctor) {
+      await doctor({
+        bot,
+        config,
+        influxDb,
+        limitDaysBack: options.limit ?? undefined,
+      });
+    } else if (options.continueMigration) {
+      await continueMigration({ bot, config, influxDb });
     } else {
       await update({ bot, config, influxDb });
     }
@@ -481,5 +759,6 @@ async function main() {
     process.exit(1);
   }
 }
+//#endregion
 
 main();
